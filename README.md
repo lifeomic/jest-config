076ac0c9c75b7f223c30d83ef5b3e217c657a8cd
David Tanner <david.tanner@lifeomic.com>
2021-08-04 10:55:24 -0600

    Initial publish setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # jest-config
 Shared Jest config utils etc.
+
+## preset
+Publish a preset file for jest. Jest [26](https://jestjs.io/docs/26.x/configuration#preset-string)  [27](https://jestjs.io/docs/configuration#preset-string)
+
+```json
+{
+  "preset": "@lifeomic/jest-config"
+}
+```
+
+Config was extracted from [lifeomic/typescript-tools](https://github.com/lifeomic/typescript-tools/blob/e38d833a27a04122856fbd5c939f2e26c9bd02d5/config/jest.js)
---
+++ jest-preset.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRunner: 'jest-circus/runner',
+  testMatch: [
+    '<rootDir>/src/**/*.test.{js,jsx,ts,tsx}',
+    '<rootDir>/test/**/*.test.{js,jsx,ts,tsx}',
+    '<rootDir>/lambda/**/*.test.{js,jsx,ts,tsx}',
+  ],
+  // Always use js and json first.  This order is used when parsing node_modules
+  // and will crash when trying to compile deps.
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'jsx', 'node', 'mjs'],
+};
